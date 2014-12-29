d7b29fedf6e1a3f903d98e4630627e78ae609be5
Krisztian Szabo <kszabo@whitepages.com>
2014-12-29 19:49:00 +0100

    Add solution

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,23 @@
 (ns alphabet-cipher.coder)
 
-(defn encode [keyword message]
-  "encodeme")
+(def alphabet (map char (range 97 123)))
+(defn- alphabet-map [coll] (zipmap alphabet coll))
 
-(defn decode [keyword message]
-  "decodeme")
+(defn- tabula-recta-row [chr]
+  (alphabet-map
+    (take (count alphabet) (drop-while (complement #{chr}) (cycle alphabet)))))
+
+(def tabula-recta (alphabet-map (map tabula-recta-row alphabet)))
+
+(defn- alphabet-cipher [f secret message]
+  (apply str (map f message (cycle secret))))
+
+(defn- lookup [row col]
+  (get-in tabula-recta [row col]))
+
+(defn- reverse-lookup [chr row-key]
+  (let [row (tabula-recta row-key)]
+    (some #(if (#{chr} (row %)) %) (keys row))))
+
+(def encode (partial alphabet-cipher lookup))
+(def decode (partial alphabet-cipher reverse-lookup))
