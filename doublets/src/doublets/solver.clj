e05610c7a8e8c38544703d7370a5dee988f2c11d
Krisztian Szabo <kszabo@whitepages.com>
2015-01-14 14:14:02 -0800

    Solve doublets

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -7,5 +7,28 @@
                (slurp)
                (read-string)))
 
-(defn doublets [word1 word2]
-  "make me work")
+(defn- word-distance [w1 w2]
+  (- (max (count w1) (count w2))
+     (count (filter identity (map = w1 w2)))))
+
+(defn- single-distance? [w other]
+  (= (word-distance w other) 1))
+
+(defn- expand [dict path]
+  (let [word (last path)
+        seen (into #{} path)]
+    (->> dict
+         (filter (complement seen))
+         (filter (partial single-distance? word))
+         (map #(conj path %)))))
+
+(defn- expansions [dict word]
+  (tree-seq (constantly true)
+            (partial expand dict)
+            [word]))
+
+(defn doublets [from target]
+  (let [matches (filter #(= target (last %)) (expansions words from))]
+    (if-let [shortest (first (sort-by count matches))]
+      shortest
+      [])))
