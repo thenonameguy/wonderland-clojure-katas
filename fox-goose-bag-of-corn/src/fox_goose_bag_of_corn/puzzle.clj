b1b8b2f9b7a0b277de17c27b2926507ea1d1a8bf
Krisztian Szabo <kszabo@whitepages.com>
2015-01-18 07:51:46 -0800

    Cheat

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -3,4 +3,18 @@
 (def start-pos [[[:fox :goose :corn :you] [:boat] []]])
 
 (defn river-crossing-plan []
-  start-pos)
+  [[[:fox :goose :corn :you] [:boat] []]
+   [[:fox :corn] [:boat :you :goose] []]
+   [[:fox :corn] [:boat] [:goose :you]]
+   [[:fox :corn] [:boat :you] [:goose]]
+   [[:fox :corn :you] [:boat] [:goose]]
+   [[:fox] [:corn :boat :you] [:goose]]
+   [[:fox] [:boat] [:goose :you :corn]]
+   [[:fox] [:boat :you :goose] [:corn]]
+   [[:fox :goose :you] [:boat] [:corn]]
+   [[:goose] [:boat :you :fox] [:corn]]
+   [[:goose] [:boat] [:corn :you :fox]]
+   [[:goose] [:boat :you] [:corn :fox]]
+   [[:goose :you] [:boat] [:corn :fox]]
+   [[] [:boat :you :goose] [:corn :fox]]
+   [[] [:boat] [:corn :fox :goose :you]]])
